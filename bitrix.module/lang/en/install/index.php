3f9cd8933e52b2eef7348955d174fba6644d7465
Elena Simchenko <33508894+php-evs@users.noreply.github.com>
2022-04-05 17:27:13 +0300

    Update index.php

--- bitrix.module/lang/en/install/index.php
+++ bitrix.module/lang/en/install/index.php
@@ -3,11 +3,11 @@
 require_once __DIR__ .'/../../../helper.php';
 $LOC = bx_loc_prefix();
 
-$MESS[$LOC .'MODULE_NAME'] = 'Google ReCaptcha';
-$MESS[$LOC .'MODULE_DESC'] = 'Google ReCaptcha v2 and v3';
-$MESS[$LOC .'PARTNER_NAME'] = 'b01110011';
-$MESS[$LOC .'PARTNER_URI'] = 'https://github.com/b01110011/';
+$MESS[$LOC .'MODULE_NAME'] = '';
+$MESS[$LOC .'MODULE_DESC'] = '';
+$MESS[$LOC .'PARTNER_NAME'] = '';
+$MESS[$LOC .'PARTNER_URI'] = '';
 
-$MESS[$LOC .'INSTALL_ERROR_VERSION'] = 'Installation error. D7 kernel version required';
-$MESS[$LOC .'INSTALL_TITLE'] = 'Install Google ReCaptcha';
-$MESS[$LOC .'UNINSTALL_TITLE'] = 'Delete Google ReCaptcha';
+$MESS[$LOC .'INSTALL_ERROR_VERSION'] = '';
+$MESS[$LOC .'INSTALL_TITLE'] = 'Install';
+$MESS[$LOC .'UNINSTALL_TITLE'] = 'Delete';
