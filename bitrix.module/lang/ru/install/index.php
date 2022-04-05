bb377c5bc083b5a137f316a2871bb49024181e57
Elena Simchenko <33508894+php-evs@users.noreply.github.com>
2022-04-05 17:27:48 +0300

    Update index.php

--- bitrix.module/lang/ru/install/index.php
+++ bitrix.module/lang/ru/install/index.php
@@ -3,11 +3,11 @@
 require_once __DIR__ .'/../../../helper.php';
 $LOC = bx_loc_prefix();
 
-$MESS[$LOC .'MODULE_NAME'] = 'Google ReCaptcha';
-$MESS[$LOC .'MODULE_DESC'] = 'Google ReCaptcha v2 и v3.'. PHP_EOL .'Защита от спама.';
-$MESS[$LOC .'PARTNER_NAME'] = 'b01110011';
-$MESS[$LOC .'PARTNER_URI'] = 'https://github.com/b01110011/';
+$MESS[$LOC .'MODULE_NAME'] = '';
+$MESS[$LOC .'MODULE_DESC'] = '';
+$MESS[$LOC .'PARTNER_NAME'] = '';
+$MESS[$LOC .'PARTNER_URI'] = '';
 
-$MESS[$LOC .'INSTALL_ERROR_VERSION'] = 'Ошибка при установки. Необходима версия ядра D7';
-$MESS[$LOC .'INSTALL_TITLE'] = 'Установка Google ReCaptcha';
-$MESS[$LOC .'UNINSTALL_TITLE'] = 'Удаление Google ReCaptcha';
+$MESS[$LOC .'INSTALL_ERROR_VERSION'] = '';
+$MESS[$LOC .'INSTALL_TITLE'] = 'Установка';
+$MESS[$LOC .'UNINSTALL_TITLE'] = 'Удаление';
